bf096fd9b3c85e16328db13f81dcdfdf47055433
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-10-20 05:30:38 +0100

    working with number and booleans in ts

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Typescript........');
+//Core Types
+//Numbers, Strings, Booleans
+function add(num1, num2, showResult) {
+    if (showResult) {
+        console.log(num1 + num2);
+    }
+    else {
+        return num1 + num2;
+    }
+}
+var number1 = 3;
+var number2 = 12;
+var printResult = true;
+var result = add(number1, number2, printResult);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('Typescript!!!!');
+//Core Types
+//Numbers, Strings, Booleans
+
+function add(num1: number, num2: number, showResult: boolean){
+  if(showResult){
+    console.log(num1 + num2);
+  } else {
+    return num1 + num2;
+  }
+}
+
+const number1 = 3;
+const number2 = 12;
+const printResult = true;
+
+const result = add(number1, number2, printResult);
+console.log(result)
