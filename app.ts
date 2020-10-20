5c9ea273a1b9cc35c86c98ba544daa1000235ca3
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-10-20 04:49:59 +0100

    add npm scripts lite-server

--- app.ts
+++ app.ts
@@ -1,15 +1,1 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-function add(num1: number, num2: number) {
-  if(typeof input1 === 'number' && typeof input2 === 'number'){
-    return num1 + num2;
-  } else {
-    return +num1 + +num2;
-  }
-}
-
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+console.log('Typescript!!!!');
